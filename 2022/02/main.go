458ef2f70fe15709f8f2f093f3539a3acb0bc10c
Matthew Chivers <matthewchivers@outlook.com>
2022-12-02 16:43:12 +0000

    Optimise Part 1 for bytes

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -14,21 +14,18 @@ func main() {
 	for _, line := range lines {
 		opponentSelection := bytes.IndexByte(points, line[0])%3 + 1
 		mySelection := bytes.IndexByte(points, line[2])%3 + 1
-		totalScore += getScore(mySelection, opponentSelection)
+		getScore(&mySelection, &opponentSelection, &totalScore)
 	}
 	log.Println("Total score:", totalScore)
-
 }
 
-func getScore(mySelection, opponentSelection int) int {
-	score := 0
+func getScore(mySelection, opponentSelection, score *int) {
 	if mySelection == opponentSelection {
-		score += 3 // draw
-	} else if mySelection == 1 && opponentSelection == 3 ||
-		mySelection == 2 && opponentSelection == 1 ||
-		mySelection == 3 && opponentSelection == 2 {
-		score += 6 // win
+		*score += 3 // draw
+	} else if *mySelection == 1 && *opponentSelection == 3 ||
+		*mySelection == 2 && *opponentSelection == 1 ||
+		*mySelection == 3 && *opponentSelection == 2 {
+		*score += 6 // win
 	} // else lose
-	score += mySelection
-	return score
+	*score += *mySelection
 }
