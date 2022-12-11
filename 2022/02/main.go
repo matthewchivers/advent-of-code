fe9859c64ebfe5b378d0bddf0bd8ffb9357986cc
Matthew Chivers <matthewchivers@outlook.com>
2022-12-11 17:09:59 +0000

    added day two part two 2022

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -8,31 +8,68 @@ import (
 )
 
 var (
-	points = []byte("ABCXYZ")
-	lines  = aoc.ReadLines("input.txt")
+	points                = []byte("ABCXYZ")
+	lines                 = aoc.ReadLines("input.txt")
+	rock, paper, scissors = 1, 2, 3
 )
 
 func main() {
-	log.Println("Total Score - Part One:", partOne())
+	log.Println("Total Score - Part One:", processLines(1))
+	log.Println("Total Score - Part Two:", processLines(2))
 }
 
-func partOne() int {
-	totalScore := 0
+func processLines(part int) int {
+	score := 0
 	for _, line := range lines {
-		opponentSelection := bytes.IndexByte(points, line[0])%3 + 1
-		mySelection := bytes.IndexByte(points, line[2])%3 + 1
-		getScore(&mySelection, &opponentSelection, &totalScore)
+		log.Println("Line:", string(line))
+		enemyInstruction := bytes.IndexByte(points, line[0])%3 + 1
+		myInstruction := bytes.IndexByte(points, line[2])%3 + 1
+		if part == 1 {
+			getScorePartOne(&enemyInstruction, &myInstruction, &score)
+		} else {
+			getScorePartTwo(&enemyInstruction, &myInstruction, &score)
+		}
 	}
-	return totalScore
+	return score
 }
 
-func getScore(mySelection, opponentSelection, score *int) {
-	if *mySelection == *opponentSelection {
+func getScorePartOne(enemyInstruction, myInstruction, score *int) {
+	if *myInstruction == *enemyInstruction {
 		*score += 3 // draw
-	} else if *mySelection == 1 && *opponentSelection == 3 ||
-		*mySelection == 2 && *opponentSelection == 1 ||
-		*mySelection == 3 && *opponentSelection == 2 {
+	} else if *myInstruction == 1 && *enemyInstruction == 3 ||
+		*myInstruction == 2 && *enemyInstruction == 1 ||
+		*myInstruction == 3 && *enemyInstruction == 2 {
 		*score += 6 // win
 	} // else lose
-	*score += *mySelection
+	*score += *myInstruction
+}
+
+func getScorePartTwo(enemyInstruction, myInstruction, score *int) {
+	switch *myInstruction {
+	case 1:
+		// I need to lose
+		switch *enemyInstruction {
+		case rock:
+			*score += scissors
+		case paper:
+			*score += rock
+		case scissors:
+			*score += paper
+		}
+	case 2:
+		// I need to draw
+		*score += 3
+		*score += *enemyInstruction
+	case 3:
+		// I need to win
+		*score += 6
+		switch *enemyInstruction {
+		case rock:
+			*score += paper
+		case paper:
+			*score += scissors
+		case scissors:
+			*score += rock
+		}
+	}
 }
