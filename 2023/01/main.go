61f835bc8581ef15cfe6783d2d2e42a9e72feb6e
Matthew Chivers <matthewchivers@outlook.com>
2023-12-01 00:17:57 +0000

    setup for 2023 day 1

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -7,9 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, advent of code 2023!")
+	fmt.Println("Hello, advent of code 2023 - Day 1!")
+	fmt.Println("Part one:", partOne())
+	fmt.Println("Part two: Not yet implemented")
+}
+
+func partOne() int {
 	lines := utils.ReadFileAsString("input.txt")
 	for _, line := range lines {
 		fmt.Println(line)
 	}
+	return 0
 }
---
+++ 2023/01/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculatePartOne(t *testing.T) {
+	// expected :=
+	expected := 0
+	// result := partOne()
+	result := 0
+
+	assert.Equal(t, expected, result, "partOne() should return the correct value")
+}
