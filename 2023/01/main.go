cd209c09708cb599272f9ad1820119df60e85e2e
Matthew Chivers <matthewchivers@outlook.com>
2023-12-01 12:00:00 +0000

    day 1 solution

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -2,20 +2,63 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/matthewchivers/advent-of-code/utils"
+)
+
+var (
+	numberMap = map[string]string{
+		"one": "1", "two": "2", "three": "3",
+		"four": "4", "five": "5", "six": "6",
+		"seven": "7", "eight": "8", "nine": "9",
+	}
 )
 
 func main() {
 	fmt.Println("Hello, advent of code 2023 - Day 1!")
 	fmt.Println("Part one:", partOne())
-	fmt.Println("Part two: Not yet implemented")
+	fmt.Println("Part two:", partTwo())
 }
 
 func partOne() int {
+	return loopLinesAndChars(1)
+}
+
+func partTwo() int {
+	return loopLinesAndChars(2)
+}
+
+func loopLinesAndChars(part int) int {
+	sum := 0
 	lines := utils.ReadFileAsString("input.txt")
 	for _, line := range lines {
-		fmt.Println(line)
+		var first, last string
+		for i, character := range line {
+			detectedChar := ""
+			if character >= '0' && character <= '9' {
+				detectedChar = string(character)
+			}
+			if part == 2 {
+				for word, number := range numberMap {
+					if strings.HasPrefix(line[i:], word) {
+						detectedChar = number
+						break
+					}
+				}
+			}
+			if detectedChar != "" {
+				if first == "" {
+					first = string(detectedChar)
+				}
+				last = string(detectedChar)
+			}
+		}
+		if integer, err := utils.StringToInt(first + last); err != nil {
+			fmt.Println(err)
+		} else {
+			sum += integer
+		}
 	}
-	return 0
+	return sum
 }
--- 2023/01/main_test.go
+++ 2023/01/main_test.go
@@ -7,10 +7,15 @@ import (
 )
 
 func TestCalculatePartOne(t *testing.T) {
-	// expected :=
-	expected := 0
-	// result := partOne()
-	result := 0
+	expected := 55488
+	result := partOne()
 
 	assert.Equal(t, expected, result, "partOne() should return the correct value")
+}
+
+func TestCalculatePartTwo(t *testing.T) {
+	expected := 55614
+	result := partTwo()
+
+	assert.Equal(t, expected, result, "partTwo() should return the correct value")
 }
