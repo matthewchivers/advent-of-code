b563cba72fb51c03af90cbc573a0a1177718ea3a
Matthew Chivers <matthewchivers@outlook.com>
2023-12-03 00:24:02 +0000

    prep for day 3

---
+++ 2023/03/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, advent of code 2023 - Day 3!")
+	fmt.Println("Part one:", partOne())
+	fmt.Println("Part two:", partTwo())
+}
+
+// partOne returns the answer to part one of the day's puzzle
+func partOne() int {
+	return -1
+}
+
+// partTwo returns the answer to part two of the day's puzzle
+func partTwo() int {
+	return -1
+}
---
+++ 2023/03/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculatePartOne(t *testing.T) {
+	expected := 0
+	result := partOne()
+
+	assert.Equal(t, expected, result, "partOne() should return the correct value")
+}
+
+// func TestCalculatePartTwo(t *testing.T) {
+// 	expected := 0
+// 	result := partTwo()
+
+// 	assert.Equal(t, expected, result, "partTwo() should return the correct value")
+// }
