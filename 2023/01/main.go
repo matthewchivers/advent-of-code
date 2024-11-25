95812321b7aa184af29e45038a581ccac5a257e4
Matthew Chivers <matthewchivers@outlook.com>
2024-11-25 17:07:48 +0000

    2023/01 refactor (comments and a 3x speedup for word matching)

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -13,7 +13,16 @@ var (
 		"four": "4", "five": "5", "six": "6",
 		"seven": "7", "eight": "8", "nine": "9",
 	}
+	startsWith = map[byte][]string{}
+	endsWith   = map[byte][]string{}
 )
+
+func init() {
+	for word := range numberMap {
+		startsWith[word[0]] = append(startsWith[word[0]], word)
+		endsWith[word[len(word)-1]] = append(endsWith[word[len(word)-1]], word)
+	}
+}
 
 func main() {
 	fmt.Println("Hello, advent of code 2023 - Day 1!")
@@ -21,39 +30,22 @@ func main() {
 	fmt.Println("Part two:", partTwo())
 }
 
+// partOne returns the sum of the first and last numbers (digits) in each line
 func partOne() int {
 	return loopLinesAndChars(1)
 }
 
+// partTwo returns the sum of the first and last numbers (digits or words) in each line
 func partTwo() int {
 	return loopLinesAndChars(2)
 }
 
+// loopLinesAndChars loops through the lines and characters of the input file
 func loopLinesAndChars(part int) int {
 	sum := 0
 	lines := util.ReadFileAsLines("input.txt")
 	for _, line := range lines {
-		var first, last string
-		for i, character := range line {
-			detectedChar := ""
-			if character >= '0' && character <= '9' {
-				detectedChar = string(character)
-			}
-			if part == 2 {
-				for word, number := range numberMap {
-					if strings.HasPrefix(line[i:], word) {
-						detectedChar = number
-						break
-					}
-				}
-			}
-			if detectedChar != "" {
-				if first == "" {
-					first = string(detectedChar)
-				}
-				last = string(detectedChar)
-			}
-		}
+		first, last := extractFirstLast(line, part)
 		if integer, err := util.StringToInt(first + last); err != nil {
 			fmt.Println(err)
 		} else {
@@ -61,4 +53,43 @@ func loopLinesAndChars(part int) int {
 		}
 	}
 	return sum
+}
+
+// extractFirstLast extracts the first and last number from a string
+// part 1: numbers are digits ("1", "2", "3")
+// part 2: numbers are digits ("1", "2", "3") or words ("one", "two", "three")
+func extractFirstLast(line string, part int) (string, string) {
+	first, last := "", ""
+	for iLeft, iRight := 0, len(line)-1; iLeft <= len(line) && iRight >= 0; iLeft, iRight = iLeft+1, iRight-1 {
+		if first == "" {
+			if line[iLeft] >= '0' && line[iLeft] <= '9' {
+				first = string(line[iLeft])
+			} else if part == 2 {
+				if words, ok := startsWith[line[iLeft]]; ok {
+					for _, word := range words {
+						if strings.HasPrefix(line[iLeft:], word) {
+							first = numberMap[word]
+						}
+					}
+				}
+			}
+		}
+		if last == "" {
+			if line[iRight] >= '0' && line[iRight] <= '9' {
+				last = string(line[iRight])
+			} else if part == 2 {
+				if words, ok := endsWith[line[iRight]]; ok {
+					for _, word := range words {
+						if strings.HasSuffix(line[:iRight+1], word) {
+							last = numberMap[word]
+						}
+					}
+				}
+			}
+		}
+		if first != "" && last != "" {
+			break
+		}
+	}
+	return first, last
 }
