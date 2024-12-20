5e4f765a2d9f343b399aabe28b5803cfdec9ea23
Matthew Chivers <matthewchivers@outlook.com>
2024-12-20 10:54:42 +0000

    changed tests to take input.txt

--- 2024/01/main_test.go
+++ 2024/01/main_test.go
@@ -7,30 +7,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPartOne(t *testing.T) {
+func TestPartOneSample(t *testing.T) {
 	lines := aoc.ReadFileAsLines("sample.txt")
 	expected := 11
 	result := partOne(lines)
 	assert.Equal(t, expected, result, "partOne() should return the correct value")
 }
 
-func TestPartTwo(t *testing.T) {
+func TestPartTwoSample(t *testing.T) {
 	lines := aoc.ReadFileAsLines("sample.txt")
 	expected := 31
 	result := partTwo(lines)
 	assert.Equal(t, expected, result, "partTwo() should return the correct value")
 }
 
+func TestPartOneInput(t *testing.T) {
+	lines := aoc.ReadFileAsLines("input.txt")
+	expected := 1506483
+	result := partOne(lines)
+	assert.Equal(t, expected, result, "partOne() should return the correct value")
+}
+
+func TestPartTwoInput(t *testing.T) {
+	lines := aoc.ReadFileAsLines("input.txt")
+	expected := 23126924
+	result := partTwo(lines)
+	assert.Equal(t, expected, result, "partTwo() should return the correct value")
+}
+
 func BenchmarkPartOne(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		lines := aoc.ReadFileAsLines("sample.txt")
+		lines := aoc.ReadFileAsLines("input.txt")
 		partOne(lines)
 	}
 }
 
 func BenchmarkPartTwo(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		lines := aoc.ReadFileAsLines("sample.txt")
+		lines := aoc.ReadFileAsLines("input.txt")
 		partTwo(lines)
 	}
 }
